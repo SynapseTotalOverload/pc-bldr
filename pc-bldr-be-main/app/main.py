6ca8004bda3b1ac4c6a7de9f4c7eebb285d4a107
srgplus <admin@srgplus.com>
2025-06-24 10:58:20 +0300

    add cors

--- pc-bldr-be-main/app/main.py
+++ pc-bldr-be-main/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.config import get_settings
 from app.api.v1 import router as products_router
+
 
 settings = get_settings()
 app = FastAPI(title=settings.app_name)
 app.include_router(products_router, prefix=settings.api_prefix)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
